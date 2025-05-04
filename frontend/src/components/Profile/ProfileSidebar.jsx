ef71d315d1c08b6c1e6b8c9f6b7f1c3079d6e672
ParthChandel165 <parth.chandel20@gmail.com>
2025-05-04 23:53:17 +0530

    fixed something

--- frontend/src/components/Profile/ProfileSidebar.jsx
+++ frontend/src/components/Profile/ProfileSidebar.jsx
@@ -22,19 +22,15 @@ const ProfileSidebar = ({ active, setActive }) => {
   const { user } = useSelector((state) => state.user);
 
   const logoutHandler = () => {
-    const navigate = useNavigate();
-  
     axios
       .get(`${server}/user/logout`, { withCredentials: true })
       .then((res) => {
-        toast.success(res.data.message); 
-        localStorage.removeItem("authToken");  
-        sessionStorage.removeItem("authToken");
+        toast.success(res.data.message);
+        window.location.reload(true);
         navigate("/login");
       })
       .catch((error) => {
         console.log(error.response.data.message);
-        toast.error("Error logging out, please try again!"); // Optional error toast
       });
   };
 
