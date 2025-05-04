ea04d5f6260cbbff1296e1e7bac7a49c755b96f7
Sravz2433 <160760540+Sravz2433@users.noreply.github.com>
2025-05-04 19:30:43 +0530

    Revert "Merge branch 'main' of https://github.com/ParthChandel165/BookCon"
    This reverts commit b2a22d91aa09046c7155bf335e94ef6ed4e98599, reversing
    changes made to f9cda6ae5eb32c913f9f5b165f02930f8a0a510a.

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,3 +1,3 @@
-export const server = `${process.env.NODE_ENV === "PRODUCTION" ? "https://bookcon-backend.onrender.com" : "http://localhost:8000"}/api/v2`;
+export const server = "http://localhost:8000/api/v2";
 
-export const backend_url = `${process.env.NODE_ENV === "PRODUCTION" ? "https://bookcon-backend.onrender.com" : "http://localhost:8000"}/`;
+export const backend_url = "http://localhost:8000/";
