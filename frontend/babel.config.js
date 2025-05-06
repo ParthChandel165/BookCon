27915637554fabbd91f3cc2e7d55e698b9c73d78
Amodinii <apamodini@gmail.com>
2025-05-07 02:32:01 +0530

    Suite resolved 4

--- frontend/babel.config.js
+++ frontend/babel.config.js
@@ -2,7 +2,9 @@ module.exports = {
   "presets": [
     ["@babel/preset-react", { "runtime": "automatic" }],
     "@babel/preset-env"
-  ]
+  ],
+  "plugins": ["babel-plugin-transform-import-meta"]
 }
+
 
   
