d338e70b6655bbdcb046c94d22114969d733874c
Amodinii <apamodini@gmail.com>
2025-05-07 02:41:01 +0530

    Suite resolved 4

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -1,15 +1,20 @@
 module.exports = {
-    testEnvironment: "jsdom",
-    transform: {
-      "^.+\\.[jt]sx?$": "babel-jest"
-    },
-    moduleNameMapper: {
-      "\\.(css|less|scss|sass)$": "identity-obj-proxy",
-      "^react-lottie$": "<rootDir>/__mocks__/react-lottie.js"
-    },
-    transformIgnorePatterns: [
-      "/node_modules/(?!(axios)/)"
-    ],
-    setupFilesAfterEnv: ["<rootDir>/jest.setup.js"]
-  };
+    "jest": {
+      "setupFilesAfterEnv": [
+        "<rootDir>/src/setupTests.js"
+      ],
+      "transform": {
+        "^.+\\.(js|jsx)$": "babel-jest"
+      },
+      "transformIgnorePatterns": [
+        "node_modules/(?!react-lottie|lottie-web)/"
+      ],
+      "moduleNameMapper": {
+        "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/src/__mocks__/fileMock.js",
+        "\\.(css|less|scss|sass)$": "<rootDir>/src/__mocks__/styleMock.js",
+        "^@/(.*)$": "<rootDir>/src/$1"
+      },
+      "testEnvironment": "jsdom"
+    }
+  }
   
