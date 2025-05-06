5759667477bfa8ecb8a3590baaf09beaec42d7f2
Amodinii <apamodini@gmail.com>
2025-05-07 01:50:24 +0530

    Suite resolved

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -5,6 +5,9 @@ module.exports = {
     },
     moduleNameMapper: {
       "\\.(css|less|scss|sass)$": "identity-obj-proxy"
-    }
+    },
+    transformIgnorePatterns: [
+      "/node_modules/(?!(axios)/)"
+    ]
   };
   
