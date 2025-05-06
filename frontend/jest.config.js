67d50090312bdfb53697e34bd06ac610ee8031d2
Amodinii <apamodini@gmail.com>
2025-05-07 01:42:28 +0530

    Ho ja

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -2,6 +2,9 @@ module.exports = {
     testEnvironment: "jsdom",
     transform: {
       "^.+\\.[jt]sx?$": "babel-jest"
+    },
+    moduleNameMapper: {
+      "\\.(css|less|scss|sass)$": "identity-obj-proxy"
     }
   };
   
