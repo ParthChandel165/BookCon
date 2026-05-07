7de1a4845f0bf58f9fa6e2df63fb51c9f5f7968d
ParthChandel165 <parth.chandel20@gmail.com>
2026-05-08 01:35:47 +0530

    debug

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,3 +1,3 @@
-export const server = "https://bookcon-backend.vercel.app/";
+export const server = "https://bookcon-backend.vercel.app/api/v2";
 
-export const backend_url = "https://bookcon-backend.vercel.app/";
+export const backend_url = "https://bookcon-backend.vercel.app";
